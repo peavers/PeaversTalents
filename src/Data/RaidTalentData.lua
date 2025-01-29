51b7d49b4124ee3eecf1682bac2b924da4b80a5b
Chris Turner <peavers@gmail.com>
2025-01-29 17:19:27 -0600

    Update src/Data/RaidTalentData.lua

--- src/Data/RaidTalentData.lua
+++ src/Data/RaidTalentData.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.raidTalents = addonTable.raidTalents or {}
 
 local talentData = {
-	updated = "2025-01-29 23:11:18", 
+	updated = "2025-01-29 23:19:25", 
 
 	[1] = {  -- Warrior
 		specs = {
