1e9f9059f298a5e62821878e57816464683948bc
Chris Turner <peavers@gmail.com>
2025-01-31 18:50:03 -0600

    Update src/Data/DungeonTalentData.lua

--- src/Data/DungeonTalentData.lua
+++ src/Data/DungeonTalentData.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.dungeonTalents = addonTable.dungeonTalents or {}
 
 local talentData = {
-	updated = "2025-02-01 00:44:34", 
+	updated = "2025-02-01 00:50:00", 
 
 	[1] = {  -- Warrior
 		specs = {
