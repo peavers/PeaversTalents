5dd6ff4f7cc1ace1b2e7044742e19691a08720c8
Chris Turner <peavers@gmail.com>
2025-02-09 12:30:03 -0600

    Update talent import codes

--- src/Data/ArchonMythicDB.lua
+++ src/Data/ArchonMythicDB.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.ArchonMythicDB = addonTable.ArchonMythicDB or {}
 
 local talentData = {
-	updated = "2025 02 09 18 11 57",
+	updated = "2025 02 09 18 29 42",
 
 	[1] = {
 		specs = {
