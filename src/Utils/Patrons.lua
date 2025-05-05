f90502088c533278faeb0ea06bc9fdb5d8f981b9
Chris Turner <peavers@gmail.com>
2025-05-05 13:54:46 -0500

    feat: Huge thanks to patreon supporters!

--- src/Utils/Patrons.lua
+++ src/Utils/Patrons.lua
@@ -8,30 +8,12 @@ addon.Patrons = Patrons
 -- Function to initialize patrons support
 function Patrons:Initialize()
     -- Ensure PeaversCommons is loaded
-    if not _G.PeaversCommons or not _G.PeaversCommons.Patrons then
+    if not _G.PeaversCommons or not _G.PeaversCommons.PatronsUI then
         return false
     end
     
-    -- If directPanel exists but supportPanel doesn't, use directPanel
-    if not addon.supportPanel and addon.directPanel then
-        addon.supportPanel = addon.directPanel
-    end
-    
-    -- Add patrons display to this addon's support UI
-    if addon.supportPanel and _G.PeaversCommons.PatronsUI then
-        _G.PeaversCommons.PatronsUI:AddToSupportPanel(addon)
-    else
-        -- Try again after a delay
-        C_Timer.After(2, function()
-            if not addon.supportPanel and addon.directPanel then
-                addon.supportPanel = addon.directPanel
-            end
-            
-            if addon.supportPanel and _G.PeaversCommons.PatronsUI then
-                _G.PeaversCommons.PatronsUI:AddToSupportPanel(addon)
-            end
-        end)
-    end
+    -- Add patrons display to this addon's settings UI
+    _G.PeaversCommons.PatronsUI:AddToSupportPanel(addon)
     
     return true
 end
